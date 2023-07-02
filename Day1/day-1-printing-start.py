1fc7a72ad1ceefa47e3e6866e78c8f01f55ca75c
DaviesAmedeus <amedeusedgar@gmail.com>
2023-07-02 23:41:30 +0300

    Day 1 completed

---
+++ Day1/day-1-printing-start.py
@@ -0,0 +1,29 @@
+# print("Hello" + " "+"Davies")
+
+#1.1 Debugging practice:
+# print("Day 1 String Manipulation")
+# print('String Concatination is done with the "+" sign.')
+# print('eg. print("Hello " + "world")')
+# print("New lines can be crated with a backslash and n.")
+
+#1.2 Input Exercise:
+# print(len(input("What is your name: ")));
+
+#1.3 Variable exercise:
+# Switching  values between varibles
+# a = input("a: ")
+# b = input("b: ")
+
+# c = a
+# a = b
+# b = c
+
+# print("a: " + a)
+# print("b: " + b)
+
+
+#1.4 Band name generator program:
+print("Welcome to Davies Band name generator program:")
+city_name = input("What is the name the city you grew up?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Name of your band should be: " + city_name + " " + pet_name + " band.")
